dd1085e9df9515b01df006874a3d73523cc89d22
Angga R <angga@linuxmail.org>
2023-03-06 04:47:48 +0700

    Idk

--- plugins/custom/ssvideo/__main__.py
+++ plugins/custom/ssvideo/__main__.py
@@ -62,7 +62,8 @@ async def ss_gen(message: Message):
     except Exception:
         command = f"vcsi -g {ss_c}x{ss_c} {vid_loc} -o ss.png"
         os.system(command)
-    doc = "ss.png"
+    dir = config.Dynamic.DOWN_PATH
+    doc = f"{dir}/ss.png"
     await message.client.send_document(
         chat_id=message.chat.id,
         document=doc)
